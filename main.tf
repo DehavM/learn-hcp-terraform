2c2c373c7077502766888b02279979fca60a7470
Marc Dehavilland <dehavm@gmail.com>
2026-06-30 17:18:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,16 +6,18 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-*"]
   }
 
   owners = ["099720109477"] # Canonical
 }
+
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
